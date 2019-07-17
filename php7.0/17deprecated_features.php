a10b9f33cbb0b3e929117703fdd75ccbb9b088c2
Cezar Rosa <cezar@nitrodev.com>
2019-07-17 01:25:20 -0300

    * deprecated: chamar método não estático de forma estática, e mais outras questões

--- php7.0/17deprecated_features.php
+++ php7.0/17deprecated_features.php
@@ -4,7 +4,8 @@ ini_set('display_errors', TRUE);
 ini_set('display_startup_errors', TRUE);
 error_reporting(E_ALL );
 
-// PHP 4 style constructors
+/* PHP 4 style constructors */
+
 //PHP 7 will emit E_DEPRECATED if a PHP 4 constructor is the only constructor defined within a class
 
 class ffff {
@@ -15,4 +16,25 @@ class ffff {
 
 $obj = new ffff();
 
+
 // resultado: deveria exibir uma mensagem deprecated
+
+/* Static calls to non-static methods */
+
+// chamar métodos não estáticos de forma estática
+
+class ggg {
+    function hhh() {
+        echo 'oi';
+    }
+}
+
+ggg::hhh();
+
+// resultado: Deprecated: Non-static method ggg::hhh() should not be called statically
+
+/* outros */
+
+// password_hash() salt option
+// capture_session_meta SSL context option ¶
+// LDAP deprecations
